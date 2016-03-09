4d28f1a38ab4b672145107e24e1d7c464bfdc463
Igor Žibert <iz6968@student.uni-lj.si>
2016-03-09 15:02:37 +0000

    skoraj

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -7,7 +7,24 @@ window.addEventListener('load', function() {
 		
 	}
 	
+	var dodajOpomnik=function(event){
+		var naziv = document.querySelector("#naziv_opomnika").value;
+		var cas = document.querySelector("#cas_opomnika").value;
+		document.querySelector("#cas_opomnika").value="";
+		document.querySelector("#naziv_opomnika").value="";
+		document.querySelector("#opomniki").innerHTML += " \
+		<div class='opomnik senca rob'> \
+            <div class='naziv_opomnika'>" + naziv + "</div> \
+            <div class='cas_opomnika'> Opomnik čez <span>" + cas + "</span> sekund.</div> \
+		</div>"
+		
+		
+		
+	}
+	
 	document.querySelector("#prijavniGumb").addEventListener("click",prijavi);
+	document.querySelector("#dodajGumb").addEventListener("click",dodajOpomnik);
+	
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
@@ -18,6 +35,13 @@ window.addEventListener('load', function() {
 			var cas = parseInt(casovnik.innerHTML);
 	
 			//TODO: 
+			if (cas > 0)  {
+				casovnik.innerHTML = cas-1;
+			} else {
+				var naziv = document.querySelector(".naziv_opomnika").innerHTML;
+				//alert
+				document.querySelector("#opomniki").removeChild(opomnik);
+			}
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
 		}
